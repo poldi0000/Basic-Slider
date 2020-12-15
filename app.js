a24cc68c2e9b9453209d89b0122472b9823d559a
Jakub podolski <jakub.podolski12@gmail.com>
2020-12-15 13:56:29 +0100

    Adding slider

--- app.js
+++ app.js
@@ -1,28 +1,33 @@
-const counter = document.querySelector('#counter')
-let number = document.querySelector('#number')
-const lowerCount = document.querySelector('#lower-count')
-const addCount = document.querySelector('#add-count')
+(function(){
+    const pics = [
+        'snatch_1',
+        'snatch_2',
+        'snatch_3',
+        'snatch_4',
+        'snatch_5',
+        'snatch_6'
+    ];
 
-total = 0;
+    const buttons = document.querySelectorAll('.btn')
+    let imageDiv = document.querySelector('.img_container')
+    let counter = 0;
+    imageDiv.style.backgroundImage = `url('./img/snatch_1.png')`
 
-function changeColor (){
-    if (total > 0){
-        number.style.color = 'green';
-    } else if (total < 0){
-        number.style.color = 'red';
-    } else {
-        number.style.color = '#505050';
-    }
-}
-
-lowerCount.addEventListener('click', () => {
-    total--;
-    number.textContent = total;
-    changeColor();
-})
-
-addCount.addEventListener('click', () => {
-    total++;
-    number.textContent = total;
-    changeColor();
-})
+    buttons.forEach(function(button){
+        button.addEventListener('click', function(e){
+            if(button.classList.contains('btn_left')){
+                counter--;
+                if(counter < 0){
+                    counter = pics.length - 1;
+                }
+            }  
+            if (button.classList.contains('btn_right')){
+                counter++;
+                if(counter > pics.length - 1){
+                    counter = 0;
+                }
+            }
+            imageDiv.style.backgroundImage = `url('./img/${pics[counter]}.png')`
+        })
+    })
+})()
